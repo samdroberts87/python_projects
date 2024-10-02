c85ee5b53782c021cd1ee3b9c8c0db1f1928fdaa
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-02 20:33:17 +0100

    first commit

--- TDD/test_is_even.py
+++ TDD/test_is_even.py
@@ -1,12 +1,14 @@
 from is_even import is_even
 import pytest
 
+
 def test_is_even():
     assert is_even(4) == True
     assert is_even(5) == False
     assert is_even(0) == True
     assert is_even(-2) == True
     assert is_even(-3) == False
+
 
 def test_is_even_invalid_input():
     with pytest.raises(TypeError):
