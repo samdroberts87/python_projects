2486de118f56220e7d4bb16dc1c282ede0d890ba
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-03 09:07:46 +0100

    reformat

--- API_projects/jokes.py
+++ API_projects/jokes.py
@@ -1,37 +1,38 @@
 import requests
 
-categories = {
-    1: "Programming",
-    2: "Miscellaneous",
-    3: "Dark",
-    4: "Pun",
-    5: "Any"
-}
+categories = {1: "Programming", 2: "Miscellaneous", 3: "Dark", 4: "Pun", 5: "Any"}
+
 
 def main():
-    
+
     chosen_category_number = get_input()
     url = get_url(chosen_category_number)
     final_joke = get_joke(url)
     print(f"\nHere's your Joke:\n\n{final_joke}\n")
 
+
 def get_input():
     ask_again = True
     while ask_again:
         print()
-        chosen_category_number = int(input(f"Choose joke type. Enter corresponding number from {categories}"))
+        chosen_category_number = int(
+            input(f"Choose joke type. Enter corresponding number from {categories}")
+        )
         if chosen_category_number <= 5 and chosen_category_number > 0:
             ask_again = False
     return chosen_category_number
-    
+
+
 def get_url(number):
     category = categories[number]
     return f"https://v2.jokeapi.dev/joke/{category}?type=single"
+
 
 def get_joke(api_url):
     response = requests.get(api_url)
     data = response.json()
     return data["joke"]
+
 
 if __name__ == "__main__":
     main()
