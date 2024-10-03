8d8ebbfd8ff61d642af32a952738f95eb570188a
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-03 18:28:25 +0100

    forgot to add line for while loop and formatted

--- API_projects/jokes.py
+++ API_projects/jokes.py
@@ -5,7 +5,7 @@ categories = {1: "Programming", 2: "Miscellaneous", 3: "Dark", 4: "Pun", 5: "Any
 
 def main():
     another_joke = True
-
+    while another_joke:
         chosen_category_number = get_input()
         url = get_url(chosen_category_number)
         final_joke = get_joke(url)
@@ -15,9 +15,13 @@ def main():
 
 def get_input():
     chosen_category_number = 0
-    while chosen_category_number not in range(1, len(categories)+1):
+    while chosen_category_number not in range(1, len(categories) + 1):
         try:
-            chosen_category_number = int(input(f"\nChoose joke type. Enter corresponding number from {categories}\n:"))
+            chosen_category_number = int(
+                input(
+                    f"\nChoose joke type. Enter corresponding number from {categories}\n:"
+                )
+            )
         except ValueError:
             print(f"\nYou must enter a number between 1 and {len(categories)}\n")
     return chosen_category_number
@@ -33,9 +37,10 @@ def get_joke(api_url):
     data = response.json()
     return data["joke"]
 
+
 def ask_for_another_joke():
     answer = input("\nWant another joke? (yes/no): ").strip().lower()
-    if answer in ['yes', 'y']:
+    if answer in ["yes", "y"]:
         return True
     else:
         print("\nThanks for using the joke app! Goodbye!\n")
