f4e1b9425366fa637885483af124cbe3cfb61a87
Sam Roberts <sam@Sams-MacBook-Air.local>
2024-10-06 15:06:55 +0100

    increase error handling scope

--- API_projects/jokes/jokes.py
+++ API_projects/jokes/jokes.py
@@ -6,14 +6,19 @@ categories = {1: "Programming", 2: "Miscellaneous", 3: "Dark", 4: "Pun", 5: "Any
 
 def main():
     another_joke = True
-    while another_joke:
-        chosen_category_number = get_input()
-        url = get_url(chosen_category_number)
-        final_joke = get_joke(url)
-        print(f"\nHere's your Joke:\n\n")
-        time.sleep(1)
-        print(f"{final_joke}\n")
-        another_joke = ask_for_another_joke()
+    try:
+        while another_joke:
+            chosen_category_number = get_input()
+            if chosen_category_number == -1:
+                break  # Break out of the loop if EOFError was encountered in get_input()
+            url = get_url(chosen_category_number)
+            final_joke = get_joke(url)
+            print(f"\nHere's your Joke:\n\n")
+            time.sleep(1)
+            print(f"{final_joke}\n")
+            another_joke = ask_for_another_joke()
+    except EOFError:
+        print("\nThanks for using the joke app! Goodbye!\n")
 
 
 def get_input():
@@ -27,6 +32,9 @@ def get_input():
             )
         except ValueError:
             print(f"\nYou must enter a number between 1 and {len(categories)}\n")
+        except EOFError:
+            print(f"\nThanks for using the joke app! Goodbye!\n")
+            return -1  # Returning a special value to indicate EOFError
     return chosen_category_number
 
 
@@ -42,10 +50,14 @@ def get_joke(api_url):
 
 
 def ask_for_another_joke():
-    answer = input("\nWant another joke? (yes/no): ").strip().lower()
-    if answer in ["yes", "y"]:
-        return True
-    else:
+    try:
+        answer = input("\nWant another joke? (yes/no): ").strip().lower()
+        if answer in ["yes", "y"]:
+            return True
+        else:
+            print("\nThanks for using the joke app! Goodbye!\n")
+            return False
+    except EOFError:
         print("\nThanks for using the joke app! Goodbye!\n")
         return False
 
